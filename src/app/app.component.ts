0c73ba4d979bff7afd94ec7c3e9fd24456e65eb2
Petyo Kostakov <petyo_kostakov@starsgroup.com>
2020-03-18 17:49:13 +0200

    From event implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,25 @@ export class AppComponent {
   title = 'Advanced-Asynchronous-JavaScript';
 
   constructor() {
-    const subscription = this.timeOut(500).subscribe({
+    // const subscription = this.timeOut(500).subscribe({
+    //   next: (value) => {
+    //     console.log('next - value', value);
+    //   },
+    //   error(e: any): void {
+    //     console.log('error');
+    //   },
+    //   complete(): void {
+    //     console.log('complete');
+    //   }
+    // });
+
+    const bodyEl: Element = document.querySelector('body');
+
+    this.fromEvent('click', bodyEl).subscribe({
       next: (value) => {
-        console.log('next - value', value);
+        console.log('next', value);
       },
-      error(error: any): void {
+      error(e: any): void {
         console.log('error');
       },
       complete(): void {
@@ -34,6 +48,22 @@ export class AppComponent {
 
       return {
         unsubscribe: () => clearTimeout(handle)
+      };
+    });
+  }
+
+  fromEvent(eventName: string, element: Element): Observable {
+    return new Observable((observer: Observer): Subscribe => {
+      const handler = (event) => {
+        observer.next(event);
+      };
+
+      element.addEventListener(eventName, handler);
+
+      return {
+        unsubscribe: () => {
+          element.removeEventListener(eventName, handler);
+        }
       };
     });
   }
